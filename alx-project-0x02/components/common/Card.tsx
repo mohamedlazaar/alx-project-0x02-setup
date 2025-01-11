9053ef2cdc84f9871ac16a685ddb9d81cda97574
mohamed lazaar <lazzarmohamed10@gmail.com>
2025-01-11 16:12:53 +0100

    creating a reusable Card Component

--- alx-project-0x02/components/common/Card.tsx
+++ alx-project-0x02/components/common/Card.tsx
@@ -2,12 +2,12 @@ import React from "react";
 import { CardProps } from "../../interfaces";
 
 
-const Card : React.FC<CardProps> = ({title, description})=>{
+const Card : React.FC<CardProps> = ({title, content})=>{
     return (
         <div className="card">
             <div className="flex flex-col w-[250px] border-white border-[1px] h-[100px] justify-center items-center">
                 <h5 className="card-title text-2xl font-bold">{title}</h5>
-                <p className="card-text">{description}</p>
+                <p className="card-text">{content}</p>
             </div>
         </div>
     )
