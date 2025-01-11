c6d03842d08cac3e9f84eada005e928289693996
mohamed lazaar <lazzarmohamed10@gmail.com>
2025-01-11 15:53:14 +0100

    basic routing in NextJs task 2

--- alx-project-0x02/components/layout/Header.tsx
+++ alx-project-0x02/components/layout/Header.tsx
@@ -1,10 +1,22 @@
 import React from 'react'
+import Link  from 'next/link'
 
 type Props = {}
 
 function Header({}: Props) {
   return (
-    <div>Header</div>
+    <div>
+        <nav className="bg-gray-800">
+            <div className="max-w-7xl mx-auto p-4">
+                <div className="flex justify-between items-center">
+                    <div className="flex space-x-4">
+                        <Link href="/home">Home</Link>
+                        <Link href="/about">About</Link>
+                    </div>
+                </div>
+            </div>
+        </nav>
+    </div>
   )
 }
 
