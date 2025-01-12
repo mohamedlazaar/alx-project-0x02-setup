92d637cf3387c1ab3870124f06b2bcb36a38ef33
mohamed lazaar <lazzarmohamed10@gmail.com>
2025-01-12 11:28:24 +0100

    implement navigation and layout

--- alx-project-0x02/pages/about.tsx
+++ alx-project-0x02/pages/about.tsx
@@ -1,15 +1,20 @@
 import React from 'react';
 import Button from '@/components/common/Button';
+import Header from '@/components/layout/Header';
 
 function about() {
   return (
-    <div className='flex flex-col gap-10 justify-center items-center'>
+    <div >
+      <Header/>
+      <div className='flex flex-col gap-10 justify-center items-center'>
         <h1 className='text-2xl p-10'>About</h1>
         <div className='flex justify-center gap-6'>
           <Button label='button Sm' size='small' shape='rounded-sm' />
           <Button label='button Md' size='medium' shape='rounded-md' />
           <Button label='button Lg' size='large' shape='rounded-full' />
-        </div>
+        </div>        
+      </div>
+
     </div>
   )
 }
---
+++ alx-project-0x02/pages/posts.tsx
@@ -0,0 +1,14 @@
+import Header from '@/components/layout/Header'
+import React from 'react'
+
+
+function posts() {
+  return (
+    <div>
+        <Header/>
+        <h1>Posts</h1>
+    </div>
+  )
+}
+
+export default posts
